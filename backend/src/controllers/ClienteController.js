810b8f2b12f46d603ef7c31a3b49cd2137139984
Wesley Lima <wesleyjoga500@gmail.com>
2021-12-28 22:40:38 -0300

    + create

--- backend/src/controllers/ClienteController.js
+++ backend/src/controllers/ClienteController.js
@@ -1,7 +1,15 @@
+const ClienteService = require('../services/ClienteService');
+
 class ClienteController {
 
-    async create() {
+    async create(data) {
+        try {
+            const cliente = await ClienteService.save(data);
 
+            return cliente;
+        } catch (error) {
+            throw error;
+        }
     }
 
     async index() {
