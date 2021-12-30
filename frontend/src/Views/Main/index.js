6c4ab2efc677acf5bda51a493b911ce93f5eacfb
Wesley Lima <wesleyjoga500@gmail.com>
2021-12-30 11:18:55 -0300

    estilização inicial do frontend

---
+++ frontend/src/Views/Main/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Content } from "./styles";
+
+export default function Main() {
+    return (
+        <Container>
+            <Content>
+                Shipancoli do Shu
+            </Content>
+        </Container>
+    )
+}
---
+++ frontend/src/Views/Main/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    padding: 15% 8% 5% 8%;
+`
+
+const Content = styled.div`
+    width: 100%;
+    padding: 2%;
+
+    text-align: center;
+`
+
+export { Container, Content };
