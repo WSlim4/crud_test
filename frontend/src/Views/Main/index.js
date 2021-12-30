96be8372eee3ed1ed96921d67f16ee471f87f2d8
Wesley Lima <wesleyjoga500@gmail.com>
2021-12-30 18:08:02 -0300

    adicionando handler para deletar cliente

--- frontend/src/Views/Main/index.js
+++ frontend/src/Views/Main/index.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { Container, Content } from "./styles";
-import mock from "../../mock";
-import Table from "../../Components/Table";
-import AddIcon from '@mui/icons-material/Add';
-import { IconButton, Tooltip } from '@mui/material';
-import ClienteOperations from '../../Operations/ClienteOperations';
 import { useSelector, useDispatch } from 'react-redux';
+import { Container, Content } from "./styles";
+import { IconButton, Tooltip } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import Table from "../../Components/Table";
 import Dialog from '../../Components/Dialog';
+import ClienteOperations from '../../Operations/ClienteOperations';
 
 export default function Main() {
 
     const dispatch = useDispatch();
-    const [open, setOpen] = useState(true);
 
-    const { value: clientes, isLoading, hasError } = useSelector((state) => state.clientes);
+    const [dialogOpen, setOpen] = useState(false);
+    const [dialogOperation, setOperation] = useState("");
+
+    const { value: clientes, isLoading, hasError, current } = useSelector((state) => state.clientes);
 
     useEffect(() => {
         fetchData();
@@ -21,20 +22,38 @@ export default function Main() {
 
     const fetchData = () => dispatch(ClienteOperations.getClientes());
 
+    const handleInfo = (id, i) => {
+        setOperation("info");
+        setOpen(true);
+        dispatch(ClienteOperations.showCliente(id, i))
+    };
+
+    const handleClose = () => setOpen(false);
+
+    const handleAdd = () => {
+        setOperation("add");
+        setOpen(true);
+    }
+
     return (
         <Container>
-            <Dialog open={open} />
+            <Dialog
+                handleClose={handleClose}
+                operation={dialogOperation}
+                open={dialogOpen}
+            />
             <Content>
                 <div className="content">
 
                     <Tooltip title="Adicionar">
-                        <IconButton>
+                        <IconButton onClick={() => handleAdd()}>
                             <AddIcon style={{ color: '#1d1e4e' }} />
                         </IconButton>
                     </Tooltip>
 
                     <div className="table-wrapper">
                         <Table
+                            handleInfo={handleInfo}
                             isLoading={isLoading}
                             hasError={hasError}
                             heads={['Nome', 'Cpf', 'E-mail', 'Celular']}
