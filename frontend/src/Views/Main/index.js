edb6afdb9024320424dd76db676a8ad8e4da838c
Wesley Lima <wesleyjoga500@gmail.com>
2021-12-30 11:51:37 -0300

    chamada do mock e do componente Table

--- frontend/src/Views/Main/index.js
+++ frontend/src/Views/Main/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Container, Content } from "./styles";
+import mock from "../../mock";
+import Table from "../../Components/Table";
 
 export default function Main() {
     return (
         <Container>
             <Content>
-                Shipancoli do Shu
+                <Table heads={['Nome', 'Cpf', 'E-mail']} users={mock.data} />
             </Content>
         </Container>
     )
