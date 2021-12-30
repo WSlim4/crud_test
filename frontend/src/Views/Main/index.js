3cc53005f5b9ee0acf7db0395c40293117a3bf33
Wesley Lima <wesleyjoga500@gmail.com>
2021-12-30 14:23:26 -0300

    + chamada do método da service

--- frontend/src/Views/Main/index.js
+++ frontend/src/Views/Main/index.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Content } from "./styles";
 import mock from "../../mock";
 import Table from "../../Components/Table";
 import AddIcon from '@mui/icons-material/Add';
 import { IconButton, Tooltip } from '@mui/material';
+import ClienteService from '../../Service/ClienteService';
 
 export default function Main() {
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const response = await ClienteService.getClientes();
+
+            console.log("Response", response);
+        } catch (error) {
+            console.log("Error", error);
+        }
+    }
+
     return (
         <Container>
             <Content>
