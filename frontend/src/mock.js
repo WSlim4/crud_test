0325accd1b23ac9c6241227d1e978d10de87ce93
Wesley Lima <wesleyjoga500@gmail.com>
2021-12-30 11:51:50 -0300

    mock temporario dos dados

---
+++ frontend/src/mock.js
@@ -0,0 +1,81 @@
+const data = {
+    "data": [
+        {
+            "endereco": {
+                "rua": "Rua da vida",
+                "bairro": "Copacabana",
+                "cidade": "Rio de Janeiro",
+                "numero": 16
+            },
+            "_id": "61cccb35f45802b473a60ce8",
+            "cpf": "55555555555",
+            "nome": "Wesley Lima",
+            "email": "wesleylima@gmail.com",
+            "celular": "21999999999",
+            "dataCriacao": "2021-12-29T20:55:17.270Z",
+            "__v": 0
+        },
+        {
+            "endereco": {
+                "rua": "Rua da vida",
+                "bairro": "Copacabana",
+                "cidade": "Rio de Janeiro",
+                "numero": 16
+            },
+            "_id": "61cccbc911c822b6acb6fa7e",
+            "cpf": "55555555555",
+            "nome": "Emuxo",
+            "email": "wesleylima@gmail.com",
+            "celular": "21999999999",
+            "dataCriacao": "2021-12-29T20:57:45.625Z",
+            "__v": 0
+        },
+        {
+            "endereco": {
+                "rua": "Rua da vida",
+                "bairro": "Copacabana",
+                "cidade": "Rio de Janeiro",
+                "numero": 16
+            },
+            "_id": "61cccbd511c822b6acb6fa80",
+            "cpf": "55555555555",
+            "nome": "Wesley Lima",
+            "email": "wesleylima@gmail.com",
+            "celular": "21999999999",
+            "dataCriacao": "2021-12-29T20:57:57.580Z",
+            "__v": 0
+        },
+        {
+            "endereco": {
+                "rua": "Rua da vida",
+                "bairro": "Copacabana",
+                "cidade": "Rio de Janeiro",
+                "numero": 16
+            },
+            "_id": "61cccbfe11c822b6acb6fa82",
+            "cpf": "55555555555",
+            "nome": "Wesley Lima",
+            "email": "wesleylima@gmail.com",
+            "celular": "21999999999",
+            "dataCriacao": "2021-12-29T20:58:38.794Z",
+            "__v": 0
+        },
+        {
+            "endereco": {
+                "rua": "Rua da vida",
+                "bairro": "Copacabana",
+                "cidade": "Rio de Janeiro",
+                "numero": 16
+            },
+            "_id": "61cd0201d55f04f79301b8a7",
+            "cpf": "55555555555",
+            "nome": "Wesley Lima 2",
+            "email": "wesleylima@gmail.com",
+            "celular": "21999999999",
+            "dataCriacao": "2021-12-30T00:49:05.262Z",
+            "__v": 0
+        }
+    ]
+}
+
+export default data;
