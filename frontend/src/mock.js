2d649f533f5020449e67136ada3978187acc1d42
Wesley Lima <wesleyjoga500@gmail.com>
2021-12-30 14:12:01 -0300

    + axios

--- frontend/src/mock.js
+++ frontend/src/mock.js
@@ -69,10 +69,40 @@ const data = {
             },
             "_id": "61cd0201d55f04f79301b8a7",
             "cpf": "55555555555",
-            "nome": "Wesley Lima 2",
-            "email": "wesleylima@gmail.com",
+            "nome": "Wesley Lima 222222222222222222222222222222",
+            "email": "wesleylima@gmail.comllllllllllllllllllll",
             "celular": "21999999999",
             "dataCriacao": "2021-12-30T00:49:05.262Z",
+            "__v": 0
+        },
+        {
+            "endereco": {
+                "rua": "Rua da vida",
+                "bairro": "Copacabana",
+                "cidade": "Rio de Janeiro",
+                "numero": 16
+            },
+            "_id": "61cccb35f45802b473a60ce8",
+            "cpf": "55555555555",
+            "nome": "Wesley Lima",
+            "email": "wesleylima@gmail.com",
+            "celular": "21999999999",
+            "dataCriacao": "2021-12-29T20:55:17.270Z",
+            "__v": 0
+        },
+        {
+            "endereco": {
+                "rua": "Rua da vida",
+                "bairro": "Copacabana",
+                "cidade": "Rio de Janeiro",
+                "numero": 16
+            },
+            "_id": "61cccbc911c822b6acb6fa7e",
+            "cpf": "55555555555",
+            "nome": "Emuxo",
+            "email": "wesleylima@gmail.com",
+            "celular": "21999999999",
+            "dataCriacao": "2021-12-29T20:57:45.625Z",
             "__v": 0
         }
     ]
