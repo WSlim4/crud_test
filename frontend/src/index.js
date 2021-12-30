b225910e9dc322f5777c67cb7648b2cf4b19c435
Wesley Lima <wesleyjoga500@gmail.com>
2021-12-30 16:56:18 -0300

    provider do redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './Store';
 import Main from './Views/Main';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Main />
+    <Provider store={store}>
+      <Main />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
