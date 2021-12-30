caeafc9e71d2184f39b0feb802f04169b8c1ad09
Wesley Lima <wesleyjoga500@gmail.com>
2021-12-30 18:07:08 -0300

    função para remover cliente

--- frontend/src/Operations/ClienteOperations.js
+++ frontend/src/Operations/ClienteOperations.js
@@ -1,5 +1,5 @@
 import ClienteService from "../Service/ClienteService";
-import { setClientes, setLoading, setError } from "../Store/Slices/clienteSlice";
+import { setClientes, setLoading, setError, setCurrent, removeCliente } from "../Store/Slices/clienteSlice";
 
 const ClienteOperations = {
     getClientes: () => async dispatch => {
@@ -14,7 +14,24 @@ const ClienteOperations = {
             dispatch(setLoading(false));
             dispatch(setError(true));
         }
+    },
+
+    showCliente: (user) => async dispatch => { dispatch(setCurrent(user)) },
+
+    deleteCliente: (id, i) => async dispatch => {
+        try {
+            const data = await ClienteService.remove(id);
+
+            if (data && data.data !== null) {
+                dispatch(removeCliente(i));
+            }
+
+        } catch (error) {
+            console.log("Error", error);
+        }
+
     }
+
 }
 
 export default ClienteOperations;
