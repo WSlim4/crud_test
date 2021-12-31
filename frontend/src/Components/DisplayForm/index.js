0e023b182582947f2c2039915e8cd1fe454ba35f
Wesley Lima (VPN) <wesley.lima@venhapranuvem.com.br>
2021-12-31 12:10:42 -0300

    estilizando o formulario de usuario

--- frontend/src/Components/DisplayForm/index.js
+++ frontend/src/Components/DisplayForm/index.js
@@ -1,26 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { DialogTitle, DialogContent, TextField } from '@mui/material';
+import './styles.css';
 
 export default function DisplayForm() {
+    const [userData, setUserData] = useState({
+        nome: "",
+        cpf: "",
+        celular: "",
+        email: "",
+        endereco: {
+            numero: "",
+            rua: "",
+            bairro: "",
+            cidade: ""
+        }
+    });
+
+    const handleChange = (e) => {
+        let { value, name} = e.target;
+        let data = {...userData};
+
+        if(name.includes('.')) {
+            name = name.split('.');
+            data[name[0]][name[1]] = value
+        } else { 
+            data[name] = value;
+        }
+
+        setUserData(data);
+
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("userData", userData);
+    }
+
     return (
         <>
             <DialogTitle>
                 Cadastrar cliente
             </DialogTitle>
-            <DialogContent style={{ textAlign: 'center', display: 'flex', justifyContent: 'space-between' }}>
-                <div>
-                    <TextField placeholder="Nome" variant="outlined" />
-                    <TextField placeholder="Cpf" variant="outlined" />
-                    <TextField placeholder="Celular" variant="outlined" />
-                    <TextField placeholder="E-mail" variant="outlined" />
-                </div>
-                <div>
-                    <TextField placeholder="UF" variant="outlined" />
-                    <TextField placeholder="Rua" variant="outlined" />
-                    <TextField placeholder="Bairro" variant="outlined" />
-                    <TextField placeholder="Cidade" variant="outlined" />
-                </div>
+            <DialogContent className="dialog">
+                <form onSubmit={handleSubmit}>
+                    <div className="inputs">
+                    <div>
+                        <TextField placeholder="Nome" name="nome" value={userData.nome} variant="outlined" onChange={handleChange}/>
+                        <TextField placeholder="Cpf" name="cpf" value={userData.cpf} variant="outlined" onChange={handleChange}/>
+                        <TextField placeholder="Celular" name="celular" value={userData.celular} variant="outlined" onChange={handleChange}/>
+                        <TextField placeholder="E-mail" name="email" value={userData.email} variant="outlined" onChange={handleChange}/>
+                    </div>
+                    <div>
+                        <TextField placeholder="Número" name="endereco.numero" value={userData.endereco.numero} variant="outlined" onChange={handleChange}/>
+                        <TextField placeholder="Rua" name="endereco.rua" value={userData.endereco.rua} variant="outlined" onChange={handleChange}/>
+                        <TextField placeholder="Bairro" name="endereco.bairro" value={userData.endereco.bairro} variant="outlined" onChange={handleChange}/>
+                        <TextField placeholder="Cidade" name="endereco.cidade" value={userData.endereco.cidade} variant="outlined" onChange={handleChange}/>
+                    </div>
+                    </div>
+                    <button type="submit">Salvar</button>
+                </form>
+                
             </DialogContent>
         </>
 
