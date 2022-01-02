a91edcfdaf6991e0fd71f638021b9bea434fa18d
WSlim4 <wesleyjoga500@gmail.com>
2022-01-02 11:36:59 -0300

    * formatando o campo numero para passar na validação do yup caso esteja vazio

--- frontend/src/Utils/format_input.js
+++ frontend/src/Utils/format_input.js
@@ -1,6 +1,8 @@
 const formatObject = (object) => {
     let obj = {...object};
-    
+    let endereco = {...obj.endereco};
+    endereco.numero = endereco.numero === "" ? 0 : endereco.numero;
+    obj.endereco = endereco;
     obj.cpf = obj.cpf.replace(/[^a-zA-Z0-9 ]/g, "");
     obj.celular = obj.celular.replace(/[^a-zA-Z0-9 ]/g, "");
 
